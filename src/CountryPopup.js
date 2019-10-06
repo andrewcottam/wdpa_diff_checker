399316f488b604d052028c5191ff624872e2e366
Andrew Cottam <a.cottam@gmail.com>
2019-10-06 15:24:24 +0000

    various stuff

--- src/CountryPopup.js
+++ src/CountryPopup.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Popup } from 'react-mapbox-gl';
 
 class CountryPopup extends React.Component {
+    clickCountryPopup(){
+      this.props.clickCountryPopup(this.props.country);
+    }
     render() {
         return (
-            <Popup coordinates={this.props.country.centroid} key={this.props.country.iso3}>
-              <div>{this.props.country.name}</div>
-              <div>New: {this.props.country.new}</div>
-              <div>Deleted: {this.props.country.deleted}</div>
+            <Popup coordinates={this.props.country.centroid} className={'countryPopup'}>
+              <div title={"View detail"} onClick={this.clickCountryPopup.bind(this)}>
+                <div className={'name'}>{this.props.country.name}</div>
+                <div className={'stats'}>
+                  <div className={''} style={{display: (this.props.country.new) ? 'inline' : 'none'}} title={this.props.country.new + ' new protected areas'}>{this.props.country.new}<span className={'stat new'}>++</span></div>
+                  <div className={''} style={{display: (this.props.country.deleted) ? 'inline' : 'none'}} title={this.props.country.deleted + ' deleted protected areas'}>{this.props.country.deleted}<span className={'stat deleted'}>--</span></div>
+                </div>
+              </div>
             </Popup>
         );
     }
 }
+
 export default CountryPopup;
