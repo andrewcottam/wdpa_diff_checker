fe2f6932ace50fa3c42eeaafe85718bbcab7b800
Andrew Cottam <a.cottam@gmail.com>
2019-10-07 17:48:37 +0000

    refactored moved mapping stuff to Map

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import jsonp from 'jsonp-promise';
 import geojson from './countries.json';
 import MyMap from './MyMap.js';
-import CountryPopup from './CountryPopup.js';
 import PAPopupList from './PAPopupList.js';
 
 const REST_BASE_URL = "https://61c92e42cb1042699911c485c38d52ae.vfs.cloud9.eu-west-1.amazonaws.com/python-rest-server/pythonrestserver/services/";
@@ -14,14 +13,10 @@ class App extends React.Component {
     super(props);
     this.state = { 
       showStatuses: ['new','deleted'], 
+      country_summary: [],
       fromVersion: "", 
       toVersion: "", 
       view: 'global', 
-      countries: [], 
-      newFilter: ["==", "wdpaid", -1], 
-      deletedFilter: ["==", "wdpaid", -1], 
-      changedFilter: ["==", "wdpaid", -1], 
-      toFilter: ["!=", "wdpaid", -1]
     };
   }
   componentDidMount() {
@@ -48,79 +43,22 @@ class App extends React.Component {
     let countriesJson = JSON.parse(JSON.stringify(geojson));
     //get the countries that have changes in this version
     this._get(REST_BASE_URL + "get_wdpa_diff_countries?format=json").then(response => {
-      let countryArray = response.records.map(country => {
+      let global_summary = response.records.map(country => {
         //find the matching item from the countries.json array
         countryData = countriesJson.features.find(feature => feature.properties.iso3 === country.iso3);
         //merge the two objects
         let returnObject = (countryData) ? Object.assign(country, countryData.properties, { "centroid": countryData.geometry.coordinates }) : country;
         return returnObject;
       });
-      //get the popups
-      this.countryPopups = this.getCountryPopups(countryArray);
-      //get the country popups
-      this.setState({countryPopups: this.countryPopups});
+      this.setState({global_summary: global_summary});
     });
-  }
-  getCountryPopups(countryArray){
-    //initialise and array to hold all the countries that have popups shown
-    let countriesWithPopups = [];
-    let countryPopups = countryArray.map(country => {
-      //the popup will be visible only if there are values for the requested type, e.g. new, deleted or changed
-      let visible = ((this.state.showStatuses.indexOf('new') !==-1)&&(country.new))||((this.state.showStatuses.indexOf('deleted') !==-1)&&(country.deleted))||((this.state.showStatuses.indexOf('changed') !==-1)&&(country.changed));
-      //add the country to the array of countries that are being shown
-      if (visible) countriesWithPopups.push(country.iso3);
-      return (visible && country.centroid) ? <CountryPopup country={country} key={country.iso3} showStatuses={this.state.showStatuses} clickCountryPopup={this.clickCountryPopup.bind(this)}/> : null;
-    });
-    //see the filter on the toLayer
-    this.setState({toFilter: ['all', ['in', 'iso3'].concat(countriesWithPopups)]});
-    return countryPopups;
-  }
-  hideCountryPopups() {
-    //hide any popups as they cause the map to lag because they are all rerendered continually
-    if (this.state.view === 'global'){
-      this.setState({ countryPopups:[] });
-    }
-  }
-  showCountryPopups() {
-    if (this.state.view === 'global'){
-      //restore the country popups
-      this.setState({countryPopups: this.countryPopups});
-    }
   }
   clickCountryPopup(country) {
-    this.hideCountryPopups();
     //set the view type
     this.setState({view: "country"});
-    //set the bounds of the map
-    this.setState({country: country, bounds: [[country.west, country.south],[country.east,country.north]]});
     //get the stats data for the country
     this._get(REST_BASE_URL + "get_wdpa_diff_country?format=json&iso3=" + country.iso3).then(response => {
-      //iterate through the country data, e.g. new: [1,2,3,4,5], deleted: [6,7,8], changed: [9.10,11]
-      if (response.records.length>0){
-        let filter, newFilter, deletedFilter, changedFilter, toFilterWDPAs = [];
-        response.records.forEach(record => {
-          filter = ['all', ['in', 'wdpaid'].concat(record.wdpaids)];
-          switch (record.status) {
-            case 'new':
-              //add the wdpaids to the new filter
-              newFilter = filter;
-              //add the new wdpas to the filter out in the to WDPA layer
-              toFilterWDPAs = toFilterWDPAs.concat(record.wdpaids);
-              break;
-            case 'deleted':
-              deletedFilter = filter;
-              break;
-            case 'changed':
-              changedFilter = filter;
-              //add the changed wdpas to the filter out in the to WDPA layer
-              toFilterWDPAs = toFilterWDPAs.concat(record.wdpaids);
-              break;
-            default:
-              // code
-            }  
-          });
-          this.setState({newFilter: newFilter, deletedFilter: deletedFilter, changedFilter: changedFilter, toFilter: ['all', ['!in', 'wdpaid'].concat(toFilterWDPAs)]});
-      }
+      this.setState({country_summary: response.records});
     });
     //get the individual changes in the protected areas
     this._get(REST_BASE_URL + "get_wdpa_diff_pas?format=json&iso3=" + country.iso3).then(response => {
@@ -135,16 +73,12 @@ class App extends React.Component {
       <React.Fragment>
         <MyMap 
           setMap={this.setMap.bind(this)}
-          hideCountryPopups={this.hideCountryPopups.bind(this)} 
-          showCountryPopups={this.showCountryPopups.bind(this)} 
-          countryPopups={this.state.countryPopups}
-          bounds={this.state.bounds} 
+          showStatuses={this.state.showStatuses}
+          global_summary={this.state.global_summary}
+          country_summary={this.state.country_summary}
+          clickCountryPopup={this.clickCountryPopup.bind(this)}
           fromVersion={this.state.fromVersion} 
           toVersion={this.state.toVersion}
-          newFilter={this.state.newFilter}
-          deletedFilter={this.state.deletedFilter}
-          changedFilter={this.state.changedFilter}
-          toFilter={this.state.toFilter}
           onMouseEnter={this.onMouseEnter.bind(this)}
           view={this.state.view}
         />
--- src/MyMap.js
+++ src/MyMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactMapboxGl, { Layer, Source, MapContext  } from 'react-mapbox-gl';
+import CountryPopup from './CountryPopup.js';
 
 const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiYmxpc2h0ZW4iLCJhIjoiMEZrNzFqRSJ9.0QBRA2HxTb8YHErUFRMPZg"
@@ -24,13 +25,98 @@ const PAINT_WDPA_DELETED = { "fill-color": "rgb(255,0,0)", "fill-outline-color":
 const PAINT_WDPA_NEW = { "fill-color": "rgb(0,255,0)", "fill-outline-color": "rgb(0,255,0)", "fill-opacity": 0.6};
 
 class MyMap extends React.Component {
-    shouldComponentUpdate(nextProps) {
-        return JSON.stringify(this.props) !== JSON.stringify(nextProps); //this is a crude hack to compare properties
+    constructor(props){
+      super(props);
+      this.state = {
+      newFilter: ["==", "wdpaid", -1], 
+      deletedFilter: ["==", "wdpaid", -1], 
+      changedFilter: ["==", "wdpaid", -1], 
+      toFilter: ["!=", "wdpaid", -1]
+      };
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        return (JSON.stringify(this.props) !== JSON.stringify(nextProps))||(JSON.stringify(this.state) !== JSON.stringify(nextState)); //this is a crude hack to compare properties
     }	
+    showCountryPopups() {
+      if (this.state.view === 'global'){
+        //restore the country popups
+        this.setState({countryPopups: this.countryPopups});
+      }
+    }
+    hideCountryPopups() {
+      //hide any popups as they cause the map to lag because they are all rerendered continually
+      if (this.state.view === 'global'){
+        this.setState({ countryPopups:[] });
+      }
+    }
+    clickCountryPopup(country){
+      //hide the country popups
+      this.hideCountryPopups();
+      //set the bounds of the map
+      this.setState({bounds: [[country.west, country.south],[country.east,country.north]]});
+      this.props.clickCountryPopup(country);
+    }
+    getCountryPopups(){
+      if (this.props.global_summary === undefined) return null;
+      //initialise and array to hold all the countries that have popups shown
+      let countriesWithPopups = [];
+      let countryPopups = this.props.global_summary.map(country => {
+        //the popup will be visible only if there are values for the requested type, e.g. new, deleted or changed
+        let visible = ((this.props.showStatuses.indexOf('new') !==-1)&&(country.new !== null))||((this.props.showStatuses.indexOf('deleted') !==-1)&&(country.deleted !== null))||((this.props.showStatuses.indexOf('changed') !==-1)&&(country.changed !== null));
+        //add the country to the array of countries that are being shown
+        if (visible) countriesWithPopups.push(country.iso3);
+        return (visible && country.centroid) ? <CountryPopup country={country} key={country.iso3} showStatuses={this.props.showStatuses} clickCountryPopup={this.clickCountryPopup.bind(this)}/> : null;
+      });
+      //set the filter on the toLayer
+      this.setState({toFilter: ['all', ['in', 'iso3'].concat(countriesWithPopups)]});
+      //return the popups
+      return countryPopups;
+    }
+    filterPAsByCountry(){
+      //iterate through the country data, e.g. new: [1,2,3,4,5], deleted: [6,7,8], changed: [9.10,11]
+      if (this.props.country_summary.length>0){
+        let filter, newFilter, deletedFilter, changedFilter, toFilterWDPAs = [];
+        this.props.country_summary.forEach(record => {
+          filter = ['all', ['in', 'wdpaid'].concat(record.wdpaids)];
+          switch (record.status) {
+            case 'new':
+              //add the wdpaids to the new filter
+              newFilter = filter;
+              //add the new wdpas to the filter out in the to WDPA layer
+              toFilterWDPAs = toFilterWDPAs.concat(record.wdpaids);
+              break;
+            case 'deleted':
+              deletedFilter = filter;
+              break;
+            case 'changed':
+              changedFilter = filter;
+              //add the changed wdpas to the filter out in the to WDPA layer
+              toFilterWDPAs = toFilterWDPAs.concat(record.wdpaids);
+              break;
+            default:
+              // code
+            }  
+          });
+          this.setState({newFilter: newFilter, deletedFilter: deletedFilter, changedFilter: changedFilter, toFilter: ['all', ['!in', 'wdpaid'].concat(toFilterWDPAs)]});
+      }
+    }
     render() {
+      let countryPopups;
+      switch (this.props.view) {
+        case 'global':
+          //get the country popups
+          countryPopups = this.getCountryPopups();
+          break;
+        case 'country':
+          //filter the PAs
+          this.filterPAsByCountry();
+          break;
+        default:
+          // code
+      }
 	    return (
 	        // eslint-disable-next-line
-            <Map style={MAP_STYLE_DEFAULT} containerStyle={{height: '100vh',width: '100vw'}} center={INITIAL_CENTER} zoom={INITIAL_ZOOM} onMoveStart={this.props.hideCountryPopups.bind(this)} onMoveEnd={this.props.showCountryPopups.bind(this)} fitBounds={this.props.bounds} fitBoundsOptions={{ padding: { top: 10, bottom: 10, left: 10, right: 10 }, easing: (num) => { return 1 }}}>
+            <Map style={MAP_STYLE_DEFAULT} containerStyle={{height: '100vh',width: '100vw'}} center={INITIAL_CENTER} zoom={INITIAL_ZOOM} onMoveStart={this.hideCountryPopups.bind(this)} onMoveEnd={this.showCountryPopups.bind(this)} fitBounds={this.state.bounds} fitBoundsOptions={{ padding: { top: 10, bottom: 10, left: 10, right: 10 }, easing: (num) => { return 1 }}}>
               <MapContext.Consumer>
                 {(map) => {
                   this.props.setMap(map);
@@ -39,12 +125,12 @@ class MyMap extends React.Component {
               <Source id={SOURCE_NAME_FROM} tileJsonSource={{type: "vector", attribution: "wdpa", tiles: [ TILES_BASE_URL + "wdpa_" + this.props.fromVersion + "_polygons&tilematrixset=EPSG:900913&Service=WMTS&Request=GetTile&Version=1.0.0&Format=application/x-protobuf;type=mapbox-vector&TileMatrix=EPSG:900913:{z}&TileCol={x}&TileRow={y}"]}}/>
               <Source id={SOURCE_NAME_TO} tileJsonSource={{type: "vector", attribution: "wdpa", tiles: [ TILES_BASE_URL + "wdpa_" + this.props.toVersion + "_polygons&tilematrixset=EPSG:900913&Service=WMTS&Request=GetTile&Version=1.0.0&Format=application/x-protobuf;type=mapbox-vector&TileMatrix=EPSG:900913:{z}&TileCol={x}&TileRow={y}"]}}/>
               <Layer id={LAYER_NAME_FROM} sourceId={SOURCE_NAME_FROM} type="fill" sourceLayer={"wdpa_" + this.props.fromVersion + "_polygons"} layout={{visibility: "none"}} paint={PAINT_WDPA_DEFAULT}/>
-              <Layer id={LAYER_NAME_TO} sourceId={SOURCE_NAME_TO} type="fill" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_DEFAULT} filter={this.props.toFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
-              <Layer id={LAYER_NAME_TO_CHANGED} sourceId={SOURCE_NAME_TO} type="fill" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_CHANGED} filter={this.props.changedFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
-              <Layer id={LAYER_NAME_FROM_DELETED} sourceId={SOURCE_NAME_FROM} type="fill" sourceLayer={"wdpa_" + this.props.fromVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_DELETED} filter={this.props.deletedFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
-              <Layer id={LAYER_NAME_TO_NEW} sourceId={SOURCE_NAME_TO} type="fill" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_NEW} filter={this.props.newFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
+              <Layer id={LAYER_NAME_TO} sourceId={SOURCE_NAME_TO} type="fill" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_DEFAULT} filter={this.state.toFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
+              <Layer id={LAYER_NAME_TO_CHANGED} sourceId={SOURCE_NAME_TO} type="fill" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_CHANGED} filter={this.state.changedFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
+              <Layer id={LAYER_NAME_FROM_DELETED} sourceId={SOURCE_NAME_FROM} type="fill" sourceLayer={"wdpa_" + this.props.fromVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_DELETED} filter={this.state.deletedFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
+              <Layer id={LAYER_NAME_TO_NEW} sourceId={SOURCE_NAME_TO} type="fill" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{visibility: "visible"}} paint={PAINT_WDPA_NEW} filter={this.state.newFilter} onMouseEnter={this.props.onMouseEnter.bind(this)}/>
               <Layer id={LAYER_NAME_TO_LABEL} sourceId={SOURCE_NAME_TO} type="symbol" sourceLayer={"wdpa_" + this.props.toVersion + "_polygons"} layout={{"visibility": (this.props.view === 'global') ? 'none' : 'visible', "text-field": "{wdpaid}", "text-size": 9}}/>
-              {this.props.countryPopups}
+              {countryPopups}
             </Map>
         );
 	}
