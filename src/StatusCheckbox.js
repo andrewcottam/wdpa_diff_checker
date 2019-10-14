9e884798019970415c5b2859a3863dc28d0b822f
Andrew Cottam <a.cottam@gmail.com>
2019-10-14 15:08:59 +0000

    css changes

--- src/StatusCheckbox.js
+++ src/StatusCheckbox.js
@@ -6,10 +6,10 @@ class StatusCheckbox extends React.Component {
     }
     render() {
         return (
-            <React.Fragment>
-                <input id={this.props.status.key + 'id'} type="checkbox" defaultChecked={this.props.status.visible} onChange={this.handleStatusChange.bind(this)}/>
+            <div className={'statusCheckboxContainer'}>
+                <input className={'statusCheckbox'} id={this.props.status.key + 'id'} type="checkbox" defaultChecked={this.props.status.visible} onChange={this.handleStatusChange.bind(this)}/>
                 <label for={this.props.status.key + 'id'}>{this.props.status.short_text}</label>
-            </React.Fragment>
+            </div>
         );
     }
 }
