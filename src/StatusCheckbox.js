ec8493a1a01d550b961fbcdc0064420b108a17e9
Andrew Cottam <a.cottam@gmail.com>
2019-10-14 07:18:37 +0000

    first build

--- src/StatusCheckbox.js
+++ src/StatusCheckbox.js
@@ -7,8 +7,8 @@ class StatusCheckbox extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <label for={this.props.status.key + 'id'}>{this.props.status.short_text}</label>
                 <input id={this.props.status.key + 'id'} type="checkbox" defaultChecked={this.props.status.visible} onChange={this.handleStatusChange.bind(this)}/>
+                <label for={this.props.status.key + 'id'}>{this.props.status.short_text}</label>
             </React.Fragment>
         );
     }
