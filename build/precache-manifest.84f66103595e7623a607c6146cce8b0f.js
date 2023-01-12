00ba4d865558e753919aa776784ae16cb961b01f
Andrew Cottam <andrew@restor.eco>
2023-01-12 10:49:01 +0000

    new build

---
+++ build/precache-manifest.84f66103595e7623a607c6146cce8b0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bcac9c6cc5f721f13bdb15bd65b340",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0fb61a9c5311f0b83856",
+    "url": "./static/css/2.6540d719.chunk.css"
+  },
+  {
+    "revision": "07820fd3e188b61bc50a",
+    "url": "./static/css/main.fd3b4407.chunk.css"
+  },
+  {
+    "revision": "0fb61a9c5311f0b83856",
+    "url": "./static/js/2.54c22014.chunk.js"
+  },
+  {
+    "revision": "e581b3ac89211983c4f9d32746545fbb",
+    "url": "./static/js/2.54c22014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07820fd3e188b61bc50a",
+    "url": "./static/js/main.ed2269ec.chunk.js"
+  },
+  {
+    "revision": "4a90c5928e11ae8d5657",
+    "url": "./static/js/runtime-main.225f5cc0.js"
+  }
+]);
